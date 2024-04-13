f18cfb40c6e8bf10a55e2213ce66c62098aeac38
Jeongbowoon <happyanne200212@gmail/com>
2024-04-13 09:33:58 +0900

    feat: to home btn 구현,  modal 구현 완료

--- week2/shopping list/index.js
+++ week2/shopping list/index.js
@@ -1,26 +1,29 @@
 const shopList = [
-    {src: "img/a1.jpg", sort:'악세서리', title: "강아지 폰케이스", price: 5900},
-    {src: "img/a2.jpg", sort:'악세서리', title: "쪼꼬미 미니 리본핀", price: 6900},
-    {src: "img/a3.jpg", sort:'악세서리', title: "행운의 네잎 클로버 지비츠", price: 12900},
-    {src: "img/s1.jpg", sort:'하의', title: "레거시 플리츠 스커트", price: 26040},
-    {src: "img/s2.jpg", sort:'하의', title: "리본 다트 밴딩 미니스커트", price: 13850},
-    {src: "img/s3.jpg", sort:'하의', title: "스위티 프릴 스커트", price: 16740},
-    {src: "img/t1.jpg", sort:'상의', title: "프릴 슈 블라우스", price: 22620},
-    {src: "img/t2.jpg", sort:'상의', title: "인디 잔꽃 원피스 세트", price: 37900},
-    {src: "img/t3.jpg", sort:'상의', title: "레이어드 뷔스티에 미니원피스", price: 16020},
-    {src: "img/t4.jpg", sort:'상의', title: "프린지 후크 트위드자켓", price: 38610},
-    {src: "img/t5.jpg", sort:'상의', title: "리본 오프숄더", price: 37640},
+    {src: "img/items/a1.jpg", sort:'악세서리', title: "강아지 폰케이스", price: 5900},
+    {src: "img/items/a2.jpg", sort:'악세서리', title: "쪼꼬미 미니 리본핀", price: 6900},
+    {src: "img/items/a3.jpg", sort:'악세서리', title: "행운의 네잎 클로버 지비츠", price: 12900},
+    {src: "img/items/s1.jpg", sort:'하의', title: "레거시 플리츠 스커트", price: 26040},
+    {src: "img/items/s2.jpg", sort:'하의', title: "리본 다트 밴딩 미니스커트", price: 13850},
+    {src: "img/items/s3.jpg", sort:'하의', title: "스위티 프릴 스커트", price: 16740},
+    {src: "img/items/t1.jpg", sort:'상의', title: "프릴 슈 블라우스", price: 22620},
+    {src: "img/items/t2.jpg", sort:'상의', title: "인디 잔꽃 원피스 세트", price: 37900},
+    {src: "img/items/t3.jpg", sort:'상의', title: "레이어드 뷔스티에 미니원피스", price: 16020},
+    {src: "img/items/t4.jpg", sort:'상의', title: "프린지 후크 트위드자켓", price: 38610},
+    {src: "img/items/t5.jpg", sort:'상의', title: "리본 오프숄더", price: 37640},
 ];
 
-//header HomeIcon Event
-const homeClickHandler = () => {
-    
-}
 
-const home = document.getElementsByClassName( "HomeIcon" );
-home[0].addEventListener( "click", homeClickHandler );
+//modal
+const modal = document.getElementsByClassName( "modal" );
+const modalOpenBtn = document.getElementById( "modalOpen" );
+modalOpenBtn.addEventListener( "click", 
+    () => { modal[0].style.visibility = "visible" }
+);
 
-
+const closeArrow = document.getElementsByClassName( "material-symbols-outlined" );
+closeArrow[0].addEventListener( "click", 
+    () => { modal[0].style.visibility = "hidden" } 
+);
 //배너 이미지 shopList array 바꾸기!
 var container = document.getElementsByClassName("banner");
 
